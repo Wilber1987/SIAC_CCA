b887f862891fe39537e840bb2125a8a92d50f296
Wilber Matus <wilberj1987@gmail.com>
2024-08-07 22:16:40 -0600

    ajustes en permisos cca

--- CAPA_NEGOCIO/Gestion_Estudiantes/Model/Parientes.cs
+++ CAPA_NEGOCIO/Gestion_Estudiantes/Model/Parientes.cs
@@ -1,30 +1,50 @@
+using API.Controllers;
 using CAPA_DATOS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace DataBaseModel {
-   public class Parientes : EntityClass {
-       [PrimaryKey(Identity = true)]
-       public int? Id { get; set; }
-       public string? Primer_nombre { get; set; }
-       public string? Segundo_nombre { get; set; }
-       public string? Primer_apellido { get; set; }
-       public string? Segundo_apellido { get; set; }
-       public string? Sexo { get; set; }
-       public string? Profesion { get; set; }
-       public string? Direccion { get; set; }
-       public string? Lugar_trabajo { get; set; }
-       public string? Telefono { get; set; }
-       public string? Celular { get; set; }
-       public string? Telefono_trabajo { get; set; }
-       public string? Email { get; set; }
-       public int? Estado_civil_id { get; set; }
-       public int? Religion_id { get; set; }
-       public DateTime? Created_at { get; set; }
-       public DateTime? Updated_at { get; set; }
-       [OneToMany(TableName = "Responsables", KeyColumn = "Id", ForeignKeyColumn = "Pariente_id")]
-       public List<Responsables>? Responsables { get; set; }
-   }
+namespace DataBaseModel
+{
+	public class Parientes : EntityClass
+	{
+		#region Propiertys
+		[PrimaryKey(Identity = true)]
+		public int? Id { get; set; }
+		public string? Primer_nombre { get; set; }
+		public string? Segundo_nombre { get; set; }
+		public string? Primer_apellido { get; set; }
+		public string? Segundo_apellido { get; set; }
+		public string? Sexo { get; set; }
+		public string? Profesion { get; set; }
+		public string? Direccion { get; set; }
+		public string? Lugar_trabajo { get; set; }
+		public string? Telefono { get; set; }
+		public string? Celular { get; set; }
+		public string? Telefono_trabajo { get; set; }
+		public string? Email { get; set; }
+		public int? Estado_civil_id { get; set; }
+		public int? Religion_id { get; set; }
+		public int? User_id { get; set; }
+		public DateTime? Created_at { get; set; }
+		public DateTime? Updated_at { get; set; }
+		[OneToMany(TableName = "Responsables", KeyColumn = "Id", ForeignKeyColumn = "Pariente_id")]
+		public List<Responsables>? Responsables { get; set; }
+		#endregion
+		
+		public static List<Estudiantes> GetOwEstudiantes(string? identity, Estudiantes estudiante)
+		{
+			UserModel user = AuthNetCore.User(identity);
+			Parientes? pariente = new Parientes().Find<Parientes>(FilterData.Equal("email", user.mail));
+			if (pariente?.Responsables != null)
+			{
+				return estudiante.Where<Estudiantes>(
+					FilterData.In("Id_Estudiante",
+				 		pariente.Responsables?.Select(r => r.Id.GetValueOrDefault()).ToArray())
+				);
+			}
+			throw new Exception("No posee estudiantes asociados");
+		}
+	}
 }
