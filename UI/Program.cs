3ab538ec078afd7d07e07b3867b867bef3bd2e18
ahernandez <iris2@softcode.es>
2024-09-26 15:58:10 -0600

    cambios

--- UI/Program.cs
+++ UI/Program.cs
@@ -12,7 +12,7 @@ using TwilioWhatsAppDemo.Services; // Asegúrate de que la ruta sea la correcta
 //SqlADOConexion.IniciarConexion("sa", "**$NIcca24@$PX", "BDSRV\\SQLCCA", "OLIMPO");
 
 //coneccion wilber
-SqlADOConexion.IniciarConexion("sa", "zaxscd", "localhost", "SIAC_CCA_TEST");
+SqlADOConexion.IniciarConexion("sa", "zaxscd", "localhost\\SQLEXPRESS", "SIAC_CCA_BEFORE_DEMO");
 //MySQLConnection.IniciarConexion("root", "", "localhost", "siac_cca_production", 3306);
 
 //coneccion alder
