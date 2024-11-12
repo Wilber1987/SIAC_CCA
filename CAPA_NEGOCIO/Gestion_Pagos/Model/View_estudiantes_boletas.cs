804e482d7efc51981ba3c075810cdf85c8a379fa
ahernandez <iris2@softcode.es>
2024-11-12 12:29:25 -0600

    correccion grado en boletas

--- CAPA_NEGOCIO/Gestion_Pagos/Model/View_estudiantes_boletas.cs
+++ CAPA_NEGOCIO/Gestion_Pagos/Model/View_estudiantes_boletas.cs
@@ -34,8 +34,8 @@ namespace CAPA_NEGOCIO.Gestion_Pagos.Model
 
         public string GetBoletasQuery()
         {
-          
-            return @"SELECT  per.idtperiodoacademico, cargo.ejercicio, cargo.tipo, cargo.idperiodoacademico, cargo.idestudiante, est.idtestudiante, cargo.periodo, cargo.idmoneda, tgm.idtmoneda, sum(cargo.importemd) as importemd, sum(cargo.descuento) as descuento, sum(cargo.importedescuentomd) as importedescuentomd
+
+            /*return @"SELECT  per.idtperiodoacademico, cargo.ejercicio, cargo.tipo, cargo.idperiodoacademico, cargo.idestudiante, est.idtestudiante, cargo.periodo, cargo.idmoneda, tgm.idtmoneda, sum(cargo.importemd) as importemd, sum(cargo.descuento) as descuento, sum(cargo.importedescuentomd) as importedescuentomd
                         , sum(cargo.importenetomd) as importenetomd, cargo.estatus,  cargo.fechagrabacion, est.idestudiante as codigo, est.nombres, est.apellidos, YEAR(current_date)as ciclo
                         , areaactual.texto as grado_actual, nactual.texto as curso_actual, areasiguiente.texto as grado_siguiente, ifnull(nsiguiente.texto,'N/D') as curso_siguiente
                             from tbl_aca_estudiantecargo cargo
@@ -57,9 +57,68 @@ namespace CAPA_NEGOCIO.Gestion_Pagos.Model
                             group by per.idtperiodoacademico, cargo.ejercicio,cargo.tipo, cargo.idperiodoacademico, cargo.idestudiante, est.idtestudiante, cargo.periodo, cargo.idmoneda,tgm.idtmoneda
                         , cargo.estatus,  cargo.fechagrabacion, est.idestudiante, est.nombres, est.apellidos, YEAR(current_date)
                         ,areaactual.texto, nactual.texto, areasiguiente.texto, nsiguiente.texto
+                            order by fechagrabacion desc";*/
+var carajo = @"SELECT  cargo.ejercicio, cargo.tipo, cargo.idperiodoacademico, cargo.idestudiante, est.idtestudiante, cargo.periodo, cargo.idmoneda, tgm.idtmoneda, sum(cargo.importemd) as importemd
+                        , sum(cargo.descuento) as descuento, sum(cargo.importedescuentomd) as importedescuentomd
+                        , sum(cargo.importenetomd) as importenetomd, cargo.estatus,  cargo.fechagrabacion, est.idestudiante as codigo, est.nombres, est.apellidos, YEAR(current_date)as ciclo
+                        ,nactual.grado grado_actual, nactual.nivel as curso_actual    ,nsiguiente.grado grado_siguiente, nsiguiente.nivel as curso_siguiente                    
+                            from tbl_aca_estudiantecargo cargo
+                                inner join tbl_aca_estudiante est on est.idestudiante = cargo.idestudiante                                
+                                inner join ( select matri.idestudiante, nactual.texto as grado, areaactual.texto as nivel from tbl_aca_matricula matri
+	                            inner  join  tbl_aca_periodoacademico per on per.idperiodoacademico = matri.idperiodoacademico
+	                            inner join tbl_aca_academianivel nactual on nactual.idacademianivel = matri.idacademianivel 
+	                            inner join tbl_aca_academiaarea areaactual on areaactual.idacademiaarea = nactual.idacademiaarea
+	                            where idestudiante = '" + this.IdTEstudiante + @"' and per.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico = " + this.Ejercicio + @"))
+	                            nactual on nactual.idestudiante = est.idestudiante 
+	                            left join ( select matri.idestudiante, nactual.texto as grado, areaactual.texto as nivel from tbl_aca_matricula matri
+	                            inner  join  tbl_aca_periodoacademico per on per.idperiodoacademico = matri.idperiodoacademico
+	                            inner join tbl_aca_academianivel nactual on nactual.idacademianivel = matri.idacademianivel 
+	                            inner join tbl_aca_academiaarea areaactual on areaactual.idacademiaarea = nactual.idacademiaarea
+	                            where idestudiante = '" + this.IdTEstudiante + @"' and per.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico = " + (this.Ejercicio + 1) + @"))
+	                            nsiguiente on nsiguiente.idestudiante = est.idestudiante 
+                                inner join tbl_gen_moneda tgm on tgm.idmoneda  = cargo.idmoneda
+                            where 	est.idtestudiante  = '" + this.IdTEstudiante + @"'
+                                and (cargo.ejercicio = " + this.Ejercicio + @" or cargo.ejercicio = " + (this.Ejercicio + 1) + @" )
+                                and idservicio in (7,2,39)                                
+                                and cargo.periodo = 1
+                                and (cargo.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico =  " + (this.Ejercicio + 1) + @")
+                                /*or cargo.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico = " + this.Ejercicio + @")*/)
+                            group by
+                            cargo.ejercicio,cargo.tipo, cargo.idperiodoacademico, cargo.idestudiante, est.idtestudiante, cargo.periodo, cargo.idmoneda,tgm.idtmoneda
+                        , cargo.estatus,  cargo.fechagrabacion, est.idestudiante, est.nombres, est.apellidos, YEAR(current_date)
+                            order by fechagrabacion desc";
+            return @"SELECT  cargo.ejercicio, cargo.tipo, cargo.idperiodoacademico, cargo.idestudiante, est.idtestudiante, cargo.periodo, cargo.idmoneda, tgm.idtmoneda, sum(cargo.importemd) as importemd
+                        , sum(cargo.descuento) as descuento, sum(cargo.importedescuentomd) as importedescuentomd
+                        , sum(cargo.importenetomd) as importenetomd, cargo.estatus,  cargo.fechagrabacion, est.idestudiante as codigo, est.nombres, est.apellidos, YEAR(current_date)as ciclo
+                        ,nactual.grado grado_actual, nactual.nivel as curso_actual    ,nsiguiente.grado grado_siguiente, nsiguiente.nivel as curso_siguiente                    
+                            from tbl_aca_estudiantecargo cargo
+                                inner join tbl_aca_estudiante est on est.idestudiante = cargo.idestudiante                                
+                                inner join ( select matri.idestudiante, nactual.texto as grado, areaactual.texto as nivel from tbl_aca_matricula matri
+	                            inner  join  tbl_aca_periodoacademico per on per.idperiodoacademico = matri.idperiodoacademico
+	                            inner join tbl_aca_academianivel nactual on nactual.idacademianivel = matri.idacademianivel 
+	                            inner join tbl_aca_academiaarea areaactual on areaactual.idacademiaarea = nactual.idacademiaarea
+	                            where idestudiante = (SELECT idestudiante from tbl_aca_estudiante where idtestudiante = '" + this.IdTEstudiante + @"')  and per.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico = " + this.Ejercicio + @"))
+	                            nactual on nactual.idestudiante = est.idestudiante 
+	                            left join ( select matri.idestudiante, nactual.texto as grado, areaactual.texto as nivel from tbl_aca_matricula matri
+	                            inner  join  tbl_aca_periodoacademico per on per.idperiodoacademico = matri.idperiodoacademico
+	                            inner join tbl_aca_academianivel nactual on nactual.idacademianivel = matri.idacademianivel 
+	                            inner join tbl_aca_academiaarea areaactual on areaactual.idacademiaarea = nactual.idacademiaarea
+	                            where idestudiante = (SELECT idestudiante from tbl_aca_estudiante where idtestudiante = '" + this.IdTEstudiante + @"') and per.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico = " + (this.Ejercicio + 1) + @"))
+	                            nsiguiente on nsiguiente.idestudiante = est.idestudiante 
+                                inner join tbl_gen_moneda tgm on tgm.idmoneda  = cargo.idmoneda
+                            where 	est.idtestudiante  = '" + this.IdTEstudiante + @"'
+                                and (cargo.ejercicio = " + this.Ejercicio + @" or cargo.ejercicio = " + (this.Ejercicio + 1) + @" )
+                                and idservicio in (7,2,39)                                
+                                and cargo.periodo = 1
+                                and (cargo.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico =  " + (this.Ejercicio + 1) + @")
+                                /*or cargo.idperiodoacademico = (SELECT idperiodoacademico from tbl_aca_periodoacademico where idtperiodoacademico = " + this.Ejercicio + @")*/)
+                            group by
+                            cargo.ejercicio,cargo.tipo, cargo.idperiodoacademico, cargo.idestudiante, est.idtestudiante, cargo.periodo, cargo.idmoneda,tgm.idtmoneda
+                        , cargo.estatus,  cargo.fechagrabacion, est.idestudiante, est.nombres, est.apellidos, YEAR(current_date)
                             order by fechagrabacion desc";
         }
-        public List<Viewestudiantesboletas> GetBoletas(){
+        public List<Viewestudiantesboletas> GetBoletas()
+        {
 
             return AdapterUtil.ConvertDataTable<Viewestudiantesboletas>(this.MTConnection?.GDatos.TraerDatosSQL(GetBoletasQuery()), this);
         }
