ded9669c86e559f35a83dfbd94e0ac5359ae8048
Wilber jose matus <wilberj@gmail.com>
2025-02-08 23:28:59 -0600

    profiles-ext/i-sadav#3946 informe

--- CAPA_NEGOCIO/Gestion_Pagos/Operations/PagosOperation.cs
+++ CAPA_NEGOCIO/Gestion_Pagos/Operations/PagosOperation.cs
@@ -33,14 +33,30 @@ namespace CAPA_NEGOCIO.Gestion_Pagos.Operations
 		public List<Tbl_Pago> GetPagos(Tbl_Pago pago, string identify)
 		{
 			//return new List<Tbl_Pago>();
-			var estudiantes = Parientes.GetOwEstudiantes(identify, new Estudiantes(),true);
+			//List<Estudiantes> estudiantes = UpdatePagosFromBellacon(identify);
+			var estudiantes = Parientes.GetOwEstudiantes(identify, new Estudiantes(), true);
+
+			return new Tbl_Pago
+			{
+				orderData = [OrdeData.Asc("Fecha")]
+			}.Where<Tbl_Pago>(
+				FilterData.In("Id_Estudiante", estudiantes.Select(x => x.Id).ToArray()),
+				FilterData.ISNull("Fecha_anulacion"),
+				FilterData.Greater("Monto_Pendiente", 0)
+			);
+
+		}
+
+		public List<Estudiantes> UpdatePagosFromBellacon(string identify)
+		{
+			var estudiantes = Parientes.GetOwEstudiantes(identify, new Estudiantes(), true);
 			var responsable = Tbl_Profile.Get_Profile(AuthNetCore.User(identify));
 			var pagosP = new Tbl_Pago()
 			{
 				orderData = [OrdeData.Asc("Fecha")]
 			}.Where<Tbl_Pago>(
-				FilterData.In("Id_Estudiante", estudiantes.Select(x => x.Id).ToArray())			
-			);			
+				FilterData.In("Id_Estudiante", estudiantes.Select(x => x.Id).ToArray())
+			);
 			List<Pagos_alumnos_view> recientraidos;
 
 
@@ -70,18 +86,9 @@ namespace CAPA_NEGOCIO.Gestion_Pagos.Operations
 			{
 				return BuildCuentasPorCobrar(x, responsable);
 			}).ToList();
-
-			return new Tbl_Pago
-			{
-				orderData = [OrdeData.Asc("Fecha")]
-			}.Where<Tbl_Pago>(
-				FilterData.In("Id_Estudiante", estudiantes.Select(x => x.Id).ToArray()),
-				FilterData.ISNull("Fecha_anulacion"),
-				FilterData.Greater("Monto_Pendiente", 0)
-			);
-
+			return estudiantes;
 		}
-		
+
 		public List<Tbl_Pago> GetPagosAllPagos(Tbl_Pago pago, string identify)
 		{
 			//return new List<Tbl_Pago>();
