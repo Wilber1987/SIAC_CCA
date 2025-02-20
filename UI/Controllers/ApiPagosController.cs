50cb88b609b12ae088c861cd428d747b333b1824
Wilber jose matus <wilberj@gmail.com>
2025-02-20 15:49:39 -0600

    profiles-ext/i-sadav#4012 ajustes en literal de autorizaciones

--- UI/Controllers/ApiPagosController.cs
+++ UI/Controllers/ApiPagosController.cs
@@ -71,6 +71,25 @@ namespace UI.Controllers
 			if (pagosResponse.status == 200) return Content((string)pagosResponse.body, "text/html");
 			else return BadRequest(pagosResponse.message);
 		}
+		
+		[HttpGet("{Id_Pago_Request}")]
+		public IActionResult GetFactura(int Id_Pago_Request)
+		{
+			try
+			{
+				// Convertir HTML a PDF utilizando wkhtmltopdf
+				byte[] pdfBytes = ApiDocumentsDataController.ConvertHtmlToPdf(PagosTemplate.GenerarFacturaHtml(new PagosRequest{Id_Pago_Request = Id_Pago_Request}.Find<PagosRequest>(), true), "A4");
+
+				// Devolver el archivo PDF como respuesta
+				return File(pdfBytes, "application/pdf", "generated.pdf");
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"Error generating PDF: {ex.Message}");
+			}
+			// Aquí puedes usar el parámetro facturaId para realizar alguna operación
+			
+		}
 
 		public class ResponseSPI
 		{
